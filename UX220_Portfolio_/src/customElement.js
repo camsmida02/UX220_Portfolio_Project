f21d2ed12d310fabd6cbdcac9c4a529526025ddc
camsmida02 <114364037+camsmida02@users.noreply.github.com>
2023-12-06 18:20:13 -0500

    Add files via upload
    Final

---
+++ UX220_Portfolio_/src/customElement.js
@@ -0,0 +1,8 @@
+//Year
+class YearElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+
+customElements.define("x-fullyear", YearElement);
